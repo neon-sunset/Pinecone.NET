069c062edb06ba179684cdb7d979daee87d8b874
neon-sunset <neon-sunset@outlook.com>
2023-04-04 05:56:34 +0300

    Work in progress...

---
+++ src/Example.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Net.Client;
+
+namespace Pinecone
+{
+    internal class Example
+    {
+        public void Test()
+        {
+            var channel = GrpcChannel.ForAddress("127.0.0.1");
+            var client = new VectorService.VectorServiceClient(channel);
+        }
+    }
+}
